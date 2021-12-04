e0d8e1a51833d94896b5df61408adc74bc38801f
JSLiroai <kat32@naver.com>
2021-12-05 01:52:28 +0900

    상단의 불필요한 흰색 박스 제거, 현재 시간의 출력이 안 보이는 오류 수정, 각 메뉴의 가격이 total에 정확히 계산되지 않는 오류 수정,하단의 버튼들을 왼쪽으로 한 칸씩 이동

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ if __name__=="__main__":
    root.geometry("1600x700+0+0")
    root.title("Restaurant Management System")
    root.resizable(0,0)
-   Tops = Frame(root,bg="white",width = 1600,height=50,relief=SUNKEN)
+   Tops = Frame(root,width = 1600,height=50,relief=SUNKEN)
    Tops.pack(side=TOP)
 
    f1 = Frame(root,width = 900,height=700,relief=SUNKEN)
@@ -25,17 +25,17 @@ if __name__=="__main__":
    txt_frame = Frame(root)
    txt_frame.pack()
 
-   txt_width = Label(txt_frame, text="현재 시간",font =('aria' ,20, 'bold'), bg="white",anchor=W)
+   txt_width = Label(txt_frame, text="현재 시간",font =('aria' ,20, 'bold'), bg="gray",anchor=W)
    txt_width.pack()
 
    clock_frame = Frame(root)
    clock_frame.pack()
 
-   clock_width = Label(clock_frame, font=("Times",30,"bold"), bg="white", bd=5)
+   clock_width = Label(clock_frame, font=("Times",30,"bold"), bg="gray", bd=5)
    clock_width.pack()
    
    #-----------------INFO TOP------------
-   lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="Restaurant Management System",fg="steel blue",bd=10,anchor='w')
+   lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="Restaurant Management System",fg="steel blue", bd=10, anchor='w')
    lblinfo.grid(row=0,column=0)
    lblinfo = Label(Tops, font=( 'aria' ,20, 'bold' ),text=clock(),fg="steel blue",anchor=W)
    lblinfo.grid(row=1,column=0)
@@ -44,25 +44,29 @@ if __name__=="__main__":
    menus = []
    rand = StringVar()
 
-
    Fries = StringVar()
    menus.append(Fries)
+
    Largefries = StringVar()
    menus.append(Largefries)
+
    Burger = StringVar()
    menus.append(Burger)
-   Filet = StringVar()
-   menus.append(Filet)
-   Service_Charge = StringVar()
-   menus.append(Service_Charge)
+
+   Pizza = StringVar()
+   menus.append(Pizza)
+
+   Spaghetti = StringVar()
+   menus.append(Spaghetti)
+
    Drinks = StringVar()
    menus.append(Drinks)
-   Cheese_burger = StringVar()
-   menus.append(Cheese_burger)
+
+   Service_Charge = StringVar()
+   menus.append(Service_Charge)
 
    Tax = StringVar()
    menus.append(Tax)
-
 
    Total = StringVar()
 
@@ -88,15 +92,15 @@ if __name__=="__main__":
    txtburger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
    txtburger.grid(row=3,column=1)
 
-   lblFilet = Label(f1, font=( 'aria' ,16, 'bold' ),text="피자",fg="steel blue",bd=10,anchor='w')
-   lblFilet.grid(row=4,column=0)
-   txtFilet = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Filet , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-   txtFilet.grid(row=4,column=1)
+   lblPizza = Label(f1, font=( 'aria' ,16, 'bold' ),text="피자",fg="steel blue",bd=10,anchor='w')
+   lblPizza.grid(row=4,column=0)
+   txtPizza = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Pizza , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+   txtPizza.grid(row=4,column=1)
 
-   lblCheese_burger = Label(f1, font=( 'aria' ,16, 'bold' ),text="스파게티",fg="steel blue",bd=10,anchor='w')
-   lblCheese_burger.grid(row=5,column=0)
-   txtCheese_burger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Cheese_burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-   txtCheese_burger.grid(row=5,column=1)
+   lblSpaghetti = Label(f1, font=( 'aria' ,16, 'bold' ),text="스파게티",fg="steel blue",bd=10,anchor='w')
+   lblSpaghetti.grid(row=5,column=0)
+   txtSpaghetti = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Spaghetti, bd=6,insertwidth=4,bg="powder blue", justify='right')
+   txtSpaghetti.grid(row=5,column=1)
 
    lblDrinks = Label(f1, font=( 'aria' ,16, 'bold' ),text="콜라",fg="steel blue",bd=10,anchor='w')
    lblDrinks.grid(row=0,column=2)
@@ -113,32 +117,32 @@ if __name__=="__main__":
    txtTotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Total , bd=6,insertwidth=10,bg="powder blue" ,justify='right')
    txtTotal.grid(row=2,column=3)
 
-   lblTotal = Label(f1,text="---------------------",fg="white")
-   lblTotal.grid(row=3,columnspan=3)
+   #lblTotal = Label(f1,text="",fg="white")
+   #lblTotal.grid(row=3,columnspan=3)
       
    #--------------------------------------------buttons--------------------------------------------
    btnTotal=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="TOTAL", bg="powder blue",command=lambda: main_frame_controller.total_button_listener(rand, menus, Total))
-   btnTotal.grid(row=7, column=1)
+   btnTotal.grid(row=7, column=0)
 
    btnreset=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="RESET", bg="powder blue",command=lambda: main_frame_controller.reset_button_listener(rand, menus, Total))
-   btnreset.grid(row=7, column=2)
+   btnreset.grid(row=7, column=1)
 
    btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="EXIT", bg="powder blue",command=lambda: main_frame_controller.exit_button_listener(root))
-   btnexit.grid(row=8, column=4)
+   btnexit.grid(row=8, column=3)
 
    btnprice=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="MENU", bg="powder blue",command=main_frame_controller.menu_button_listener)
-   btnprice.grid(row=8, column=1)
+   btnprice.grid(row=8, column=0)
 
    btnpay=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="PAY", bg="powder blue",command=main_frame_controller.pay_button_listener)
-   btnpay.grid(row=7, column=4)
+   btnpay.grid(row=7, column=3)
 
    btncalender=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="CALENDER", bg="powder blue",command=main_frame_controller.calender_button_listener)
-   btncalender.grid(row=8, column=3)
+   btncalender.grid(row=8, column=2)
 
    btncoupon=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="COUPON", bg="powder blue",command=main_frame_controller.coupon_button_listener)
-   btncoupon.grid(row=7, column=3)
+   btncoupon.grid(row=7, column=2)
 
    btnmanage=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="MANAGE", bg="powder blue",command=main_frame_controller.manage_button_litener)
-   btnmanage.grid(row=8, column=2)
+   btnmanage.grid(row=8, column=1)
 
    root.mainloop()
