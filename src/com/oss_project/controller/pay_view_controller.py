16d6905f82b596c386ec850e34bf18bdc8ef4613
JSLiroai <kat32@naver.com>
2021-12-07 21:14:07 +0900

    qr결제취소 버튼 클릭시 아무 이벤트가 발생하지 않는 현상 수정,qr결제에 대한 버튼 변수 수정

--- src/com/oss_project/controller/pay_view_controller.py
+++ src/com/oss_project/controller/pay_view_controller.py
@@ -1,4 +1,4 @@
-#모듈 연결용 임시코드
+#모듈 연결용 임시코개
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))))
@@ -6,8 +6,6 @@ import database.data as data
 from tkinter import messagebox
 import sqlite3
 from tkinter import *
-from tkinter import Tk
-import tkinter
 
 #현금 결제
 def cash_button_listener(root_view):
@@ -24,24 +22,20 @@ def qr_button_listener(root_view):
   w.title("kakao_pay")
   w.geometry("500x500")
   w.resizable(True,True)
-  
-  
-  btncancel = Button(w,text = "QR 결제 성공",fg = "white", bg = "blue" ,padx=20,pady=8, bd=10, command=lambda: query(root_view))
-  btncancel.pack(side = BOTTOM)
-  btncancel = Button(w,text = "QR 결제 취소",fg = "white", bg = "blue" ,padx=20,pady=8, bd=10)
-  btncancel.pack(side = BOTTOM)
-  
-  photo = tkinter.PhotoImage(file= "src\com\oss_project\controller\kakaopay.gif",master=w)
-  pLabel = Label(w, image = photo)
-  pLabel.pack()
+
+  btnsuccess = Button(w,text = "QR 결제 성공",fg = "white", bg = "blue" , padx = 20, pady = 8, bd = 10, command=lambda: query(root_view))
+  btnsuccess.pack()
+  btncancel = Button(w,text = "QR 결제 취소",fg = "white", bg = "blue" ,padx = 20, pady = 8, bd = 10,command=w.destroy)
+  btncancel.pack()
+
 
   w.mainloop()
-  
+
 #주문시 데이터베이스에 쿼리문 실행
 def query(root_view):
     con = sqlite3.connect(data.path)
     cur = con.cursor()
-    
+
     try:
         sql = "INSERT INTO 'order' ('ID', 'price_sum') values (\'" + data.OrderInfo.total_order_id + "\', " + str(data.OrderInfo.total_sum) + ")"
         cur.execute(sql)
@@ -49,14 +43,14 @@ def query(root_view):
           sql = "INSERT INTO 'ordered_product' ('order_id', 'product_name', 'num', 'unit_price') values (\'" + data.OrderInfo.total_order_id + "\', \'" + name + "\', " + str(num) + ", " + str(price) +")"
           cur.execute(sql)
     except Exception as e:
-        messagebox.showerror('오류', '데이터 입력 오류가 발생하였습니다.', parent=root_view)
+        messagebox.showerror('오류', '결제 오류가 발생하였습니다.', parent=root_view)
         print(e)
     else:
         data.OrderInfo.total_order_id = None
         data.OrderInfo.total_ordered_product_list = []
         data.OrderInfo.total_sum = None
-        messagebox.showinfo('성공', '데이터 입력 성공', parent=root_view)
-        
+        messagebox.showinfo('성공', '결제 성공', parent=root_view)
+
 
     con.commit()
     con.close()
